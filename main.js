65aaac5f7cfb94786942f7ab28cfe423129c6832
Dennis Kimathi <kimathidennis38@gmail.com>
2022-06-17 19:31:14 +0300

    commit tree b44f147c591e70a854a15a6c6b1b82d4a486cf47

--- main.js
+++ main.js
@@ -1,67 +1,35 @@
 // We use the Objects below to control toggling like / unlike status
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥"
 
-const glyphStates = {
-  "♡": "♥",
-  "♥": "♡"
-};
 
-const colorStates = {
-  "red" : "",
-  "": "red"
-};
 
-const modal = document.querySelector('#modal')
-
-//Remove Modal
-window.addEventListener('DOMContentLoaded', function(){
-  modal.classList.add('hidden');
-
-})
-
-// STEP 1: The line of code below is what lets JavaScript find the elements that
-// we want to make clickable. Without JavaScript, clicking on these heart shapes
-// does nothing. Uncomment the code and refresh the demo page. 
 
 const likeHearts = document.querySelectorAll(".like-glyph");
+const errorMessage = document.getElementById('modal');
 
-function likeCallback(e) {
-  const heart = e.target;
-  mimicServerCall()
-    .then(function(serverMessage){
-      // STEP 2: Uncomment the 3 lines after the alert.
-      // Here we're using Pillar 1 (DOM Manipulation) to update the screen and
-      // mimicking Pillar 3 (Server Communication) to only update the screen if
-      // the sending of information to the server succeeds.
-      alert("You notified the server!");
-      alert(serverMessage);
-      heart.innerText = glyphStates[heart.innerText];
-      heart.style.color = colorStates[heart.style.color];
+for (const like of likeHearts) {
+  like.addEventListener("click", () => {
+    mimicServerCall(url="http://mimicServer.example.com")
+    .then(() => {
+      if(like.innerHTML === EMPTY_HEART){
+        like.innerHTML = FULL_HEART;
+        like.className = "activated-heart";
+
+      }else {
+        like.innerText = ERROR;
+      }
     })
-    .catch(function(error) {
-      setTimeout(function() {
-        modal.classList.remove('hidden'), 3000
-      })
-    });
+    .catch(err => {
+      errorMessage.className = "";
+      errorMessage.innerText = err;
+      setTimeout(() => errorMessage.className = "hidden", 3000);
+
+    })
+  })
 }
 
-// STEP 3: In order for the call to the server and the update of the screen to
-// work, we need to add a click event listener to the elements we identified in
-// STEP 1. That's Pillar 2, event handling. Uncomment this code:
 
-for (const glyph of likeHearts) {
-  glyph.addEventListener("click", likeCallback);
-}
-
-// STEP 4: 
-
-// When all the STEPs' code changes have been complete, you'll be able to see a
-// working demonstration of our reference example. Sure, it's maybe not as
-// pretty as a professional site, but they're only different from our site in
-// style, not substance.
-
-//------------------------------------------------------------------------------
-// Ignore after this point. Used only for demo purposes
-//------------------------------------------------------------------------------
 
 
 
